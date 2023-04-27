de41a03271f4d8c28c5a77934bfddd789d67f334
Lazarov_O_M_QAMID_61 <oleg057orel@yandex.ru>
2023-04-27 19:10:04 +0300

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676; //Стоимость билета
+        int bonusPrice = 20; //Стоимость одного бонуса
+
+        int flightBonus;
+
+        System.out.println("Вам начислено бонусов: "+ (price / bonusPrice));
+    }
+}
